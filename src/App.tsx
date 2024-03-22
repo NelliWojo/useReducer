b7addc8e32866f44b9b0eb2bafbc8b97b72812d3
NelliWojo <120693666+Raftouch@users.noreply.github.com>
2024-03-22 13:27:25 +0100

    feat :zap: error handling

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,24 @@ function reducer(state: State, action: Action) {
 
   switch (type) {
     case "increment": {
-      return { ...state, count: state.count + 1 }; // make a copy of a state & overwrire this state with a new count value
+      // return { ...state, count: state.count + 1 }; // make a copy of a state & overwrire this state with a new count value
+      const newCount = state.count + 1;
+      const hasError = newCount > 5;
+      return {
+        ...state,
+        count: hasError ? state.count : newCount,
+        error: hasError ? "maximum reached" : null,
+      };
     }
     case "decrement": {
-      return { ...state, count: state.count - 1 };
+      // return { ...state, count: state.count - 1 };
+      const newCount = state.count - 1;
+      const hasError = newCount < 0;
+      return {
+        ...state,
+        count: hasError ? state.count : newCount,
+        error: hasError ? "minimum reached" : null,
+      };
     }
     default:
       return state;
@@ -34,7 +48,7 @@ export default function App() {
   return (
     <div>
       <div>Count: {state.count}</div>
-      {state.error && <div>{state.error}</div>}
+      {state.error && <div style={{ color: "red" }}>{state.error}</div>}
       <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
       <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
     </div>
